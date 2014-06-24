f1202031340c7bd7f63d8faa90ba9f79953a8f0a
Dzmitry Malyshau <kvarkus@gmail.com>
2014-06-24 19:32:46 -0400

    Added lib module

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![crate_id = "heterogene#0.1"]
+#![crate_type = "rlib"]
+#![comment = "Heterogeneous containers for Rust"]
+#![feature(macro_rules)]
+#![allow(uppercase_variables)]
+
+pub mod queue;
